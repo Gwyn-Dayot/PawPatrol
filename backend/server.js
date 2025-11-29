07eb19021241485023d4ce6c9f71fc9714016941
Gwyn-Dayot <20230028015@my.xu.edu.ph>
2025-11-29 23:12:56 +0800

    fix: correct API_URL protocol and improve server.js structure

--- backend/server.js
+++ backend/server.js
@@ -4,29 +4,41 @@ import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
-import auth from './routes/auth.js';
-
-dotenv.config();
-connectDB();
-
-const app = express();
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
-app.use(morgan('dev'));
-
-app.get('/', (req, res) => {
-    res.send('PAWdoption Backend is running');
-});
-
-app.use('/api/auth', auth);
-
+import authRoutes from './routes/auth.js';
 import { protect } from './middleware/auth.js';
 import { authorizeRoles } from './middleware/roles.js';
 
-app.get('/api/admin/only', protect, authorizeRoles('admin'), (req, res) => {
-    res.send({message: 'Welcome PAWdoption Admin!'});
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(express.json());
+
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
+
+
+app.get('/', (req, res) => {
+  res.send('PAWdoption Backend is running');
 });
+
+
+app.use('/api/auth', authRoutes);
+
+
+app.get('/api/admin/only', protect, authorizeRoles('admin'), (req, res) => {
+  res.send({message: 'Welcome PAWdoption Admin!'});
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
